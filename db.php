aa42d0b0df95f056c88e3c23a4791ec112ea0a60
Danijel <dan.janjic@gmail.com>
2021-03-19 18:54:55 +0100

    Dodat fajl sa sql upitima potrebnim za kreiranje i popunjavanje baze. Zamenjeni harkodovani postovim sa onima iz baze

---
+++ db.php
@@ -0,0 +1,33 @@
+<?php
+// ako su mysql username/password i ime baze na vasim racunarima drugaciji
+// obavezno ih ovde zamenite
+$server = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "blog";
+
+try {
+  $connection = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch (PDOException $e) {
+  echo $e->getMessage();
+}
+// svi postovi
+function GetAllPostsFromDB($sql, $connection)
+{
+  $statement = $connection->prepare($sql);
+  $statement->execute();
+  $statement->setFetchMode(PDO::FETCH_ASSOC);
+  return $statement->fetchAll();
+}
+//single post
+function GetSinglePostFromDB($sql, $connection)
+{
+  $statement = $connection->prepare($sql);
+  $statement->execute();
+  $statement->setFetchMode(PDO::FETCH_ASSOC);
+  return $statement->fetchAll();
+}
+
+?>
